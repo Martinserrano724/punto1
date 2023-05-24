6df614b47cd937b8b59d0a1ca7116c5fc3dcdc71
Martinserrano724 <martinserrano724@gmail.com>
2023-05-24 10:43:14 -0300

    Creacion y desarrollo del punto 1

---
+++ src/components/Ejer1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ejer1 = () => {
+    return (
+        <div>
+            <h1 className='text-center mt-5'>Hello wordld!</h1>
+        </div>
+    );
+};
+
+export default Ejer1;
