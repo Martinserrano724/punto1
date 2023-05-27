f5561993b9b0c0c3f3202dd5a7a826490e9f0fec
Martinserrano724 <martinserrano724@gmail.com>
2023-05-26 22:38:43 -0300

    Correcion en errores de maquetado

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Ejer1 from './components/Ejer1';
+
+
+function App() {
+  return (
+    <>
+     <Ejer1 />
+    </>
+  )
+}
+
+export default App
